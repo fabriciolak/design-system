a9656fea0804bd0df428d71b1201c036ba29c44f
Fabricio Silva <fabriciospfullstack@gmail.com>
2022-12-13 17:16:34 -0300

    feat: create docs for tokens

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
   ],
   "addons": [
     "@storybook/addon-links",
