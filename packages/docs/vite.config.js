e855c952b8c0837e0b676fd7fe527e8e5dd5572d
Fabricio Silva <fabriciospfullstack@gmail.com>
2022-12-15 15:49:34 -0300

    feat: turbo added, create docs for each tokens

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react],
+  plugins: [react()],
 })
