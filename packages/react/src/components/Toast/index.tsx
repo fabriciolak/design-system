22c4151f433c4fbefcb6c674c35544e5e8ae968e
Fabrício <fabriciolak12@gmail.com>
2022-12-20 22:32:20 -0300

    feat: new toast component

---
+++ packages/react/src/components/Toast/index.tsx
@@ -0,0 +1,37 @@
+import * as RadixToast from '@radix-ui/react-toast'
+import { ToastProps as RadixToastProps } from '@radix-ui/react-toast'
+import { X } from 'phosphor-react'
+
+import {
+  ToastRoot,
+  ToastTitle,
+  ToastDescription,
+  ToastViewport,
+  ToastAction,
+} from './styles'
+
+export interface ToastProps extends RadixToastProps {
+  open: boolean
+  setOpen: undefined
+}
+
+export function Toast({ open, setOpen, ...props }: ToastProps) {
+  return (
+    <RadixToast.Provider swipeDirection="right">
+      <ToastRoot open={open} {...props}>
+        <ToastTitle>Agendamento realizado</ToastTitle>
+        <ToastDescription>
+          <time>Terça-feira, 20 de Dezembro às 20hs</time>
+        </ToastDescription>
+
+        <ToastAction asChild altText="Close toast">
+          <button>
+            <X size={20} weight="bold" color="#A9A9B2" />
+          </button>
+        </ToastAction>
+      </ToastRoot>
+
+      <ToastViewport />
+    </RadixToast.Provider>
+  )
+}
---
+++ packages/react/src/components/Toast/styles.ts
@@ -0,0 +1,104 @@
+import * as Toast from '@radix-ui/react-toast'
+
+import { keyframes, styled } from '../../styles'
+
+export const ToastViewport = styled(Toast.Viewport, {
+  position: 'fixed',
+  bottom: 0,
+  right: 0,
+  width: 360,
+  maxWidth: '100vw',
+  zIndex: '999999',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 10,
+  listStyle: 'none',
+  outline: 'none',
+  padding: '$8',
+  margin: 0,
+})
+
+const hideToast = keyframes({
+  '0%': {
+    opacity: 1,
+  },
+  '100%': {
+    opacity: 0,
+  },
+})
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateX(calc(100% + 25px))',
+  },
+  to: {
+    transform: 'translateX(0%)',
+  },
+})
+
+const swipeOut = keyframes({
+  from: { transform: 'translateX(0%)' },
+  to: { transform: `translateX(calc(100% + 25px))` },
+})
+
+export const ToastRoot = styled(Toast.Root, {
+  display: 'grid',
+  gridTemplateAreas: '"title action" "description action"',
+  gridTemplateColumns: 'auto max-content',
+  padding: 20,
+  borderRadius: 6,
+  background: '$gray800',
+
+  '&[data-state="open"]': {
+    animation: `${slideIn} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+
+  '&[data-swipe="move"]': {
+    transform: 'translateX(0)',
+  },
+
+  '&[data-swipe="cancel"]': {
+    transform: 'translateX(0)',
+    transition: 'transform 200ms ease-out',
+  },
+
+  '&[data-state="closed"]': {
+    animation: `${hideToast} 100ms ease-out`,
+  },
+
+  '&[data-swipe="end"]': {
+    animation: `${swipeOut} 100ms ease-out`,
+  },
+})
+
+export const ToastTitle = styled(Toast.Title, {
+  gridArea: 'title',
+  fontFamily: '$default',
+  fontSize: '$xl',
+  fontWeight: '$bold',
+  lineHeight: '$base',
+  marginBottom: '$1',
+  color: '$white',
+})
+
+export const ToastDescription = styled(Toast.Description, {
+  gridArea: 'description',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  lineHeight: '$base',
+  color: '$gray200',
+})
+
+export const ToastAction = styled(Toast.Action, {
+  gridArea: 'action',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: 'transparent',
+  cursor: 'pointer',
+  width: '$8',
+  height: '$8',
+  border: 'none',
+  outline: 'none',
+})
